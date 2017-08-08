48a962f8b6ba873cb98997e6e48dbffc4670310f
YoProgrammer <yuttasakcom@gmail.com>
2017-08-08 18:02:18 +0700

    Add styles

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
+import '@/assets/styles/mystyle.scss'
 
 Vue.config.productionTip = false
 
